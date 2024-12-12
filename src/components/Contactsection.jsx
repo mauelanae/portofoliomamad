a328535d655a6e14fe885a2b892f251bd1ccdfed
mauelanae <mauelanaemad@gmail.com>
2024-12-12 19:46:58 +0700

    Mengubah url

--- src/components/Contactsection.jsx
+++ src/components/Contactsection.jsx
@@ -18,7 +18,7 @@ const Contactsection = forwardRef((props, ref) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:3000/api/formportofolio', {
+            const response = await fetch('https://portofolio-mamad.netlify.app/api/formportofolio', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
